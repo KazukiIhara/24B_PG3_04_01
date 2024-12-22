014fa700414bbaa3fceb83f1cf7e8fe9c877f479
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-12-23 00:12:31 +0900

    noviceResource追加

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "LE2A_01_イハラ_カズキ";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
